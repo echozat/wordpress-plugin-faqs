2fb5989aff7bdc9305dbb7e6aec8567696ebf970
thinkerkarthik <karthik.hebbar@think201.com>
2026-04-21 18:54:00 +0530

    unintall.php changes

--- uninstall.php
+++ uninstall.php
@@ -4,6 +4,10 @@ if (! defined('WP_UNINSTALL_PLUGIN')) {
     exit;
 }
 
+if (! defined('FAQS_PATH')) {
+    define('FAQS_PATH', plugin_dir_path(__FILE__));
+}
+
 require_once FAQS_PATH . 'src/core/plugin-setup.php';
 
 if (class_exists('faqs\PluginSetup') && method_exists('faqs\PluginSetup', 'delete')) {
